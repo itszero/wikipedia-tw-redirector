024fc8fe923eb82042d575d5547a5aecf0d93795
Zero Cho <itszero@gmail.com>
2020-05-18 00:45:15 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,20 @@
+const patterns = [
+  {
+    match: /https?:\/\/zh.wikipedia.org\/(wiki|zh|zh-hans|zh-hant|zh-cn|zh-hk|zh-mo|zh-my|zh-sg)\/(.+)/,
+    replace: 'https://zh.wikipedia.org/zh-tw/$2'
+  },
+]
+
+chrome.webRequest.onBeforeRequest.addListener((e) => {
+  const { tabId, url } = e
+    for (const pattern of patterns) {
+    if (url.match(pattern.match)) {
+      const newUrl = url.replace(pattern.match, pattern.replace)
+      if (url !== newUrl) {
+        return { redirectUrl: newUrl }
+      }
+    }
+  }
+}, {
+  urls: ['*://zh.wikipedia.org/*']
+}, ['blocking'])
---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+LOGO_SIZES=(48 16)
+for size in $LOGO_SIZES; do
+  echo "--> Creating logo ${size}x${size}..."
+  convert logo128.png -resize ${size}x${size} logo${size}.png
+done
+
+echo "--> Packaging to wikipedia-tw.xpi"
+rm wikipedia-tw-redirector.xpi /tmp/wikipedia-tw-redirector.xpi
+zip /tmp/wikipedia-tw-redirector.xpi *
+mv /tmp/wikipedia-tw-redirector.xpi .
